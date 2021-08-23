60b32f046bedfa7d80248de58972588bc70d02b5
Matthias Frener <mfrener@cinemo.com>
2021-08-23 19:28:41 +0200

    Change subscribeUntil cancelSignal to Observable

--- src/util/rxjs-helper.ts
+++ src/util/rxjs-helper.ts
@@ -1,8 +1,8 @@
-import {Observable, Observer, Subject, takeUntil} from "rxjs";
+import {Observable, Observer, takeUntil} from "rxjs";
 
 /** Subscribe on the given observable until a value is emiited by cancelSignal. */
 export function subscribeUntil<OBSERVABLE_TYPE, SIGNAL_TYPE>(
-  cancelSignal: Subject<SIGNAL_TYPE>,
+  cancelSignal: Observable<SIGNAL_TYPE>,
   observable: Observable<OBSERVABLE_TYPE>,
   observer: Partial<Observer<OBSERVABLE_TYPE>>,
 ): void {
