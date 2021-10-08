a7f1032ca56116edd592585fa2d14319cde39b27
Matthias Frener <mfrener@cinemo.com>
2021-10-08 17:05:13 +0200

    fix build

--- src/index.ts
+++ src/index.ts
@@ -43,10 +43,6 @@ export {HttpStatusCode} from "./models/types";
 export {WebhookSubscriptionRequest} from "./models/webhook-subscription-request";
 export {WebSocketCloseCode} from "./models/websocket-close-code";
 export {DiffTools} from "./util/diff-tools";
-export {
-  EventStreamCommand,
-  EventToStreamDispatcher,
-} from "./util/event-to-stream-dispatcher";
 export {MapExt} from "./util/map-ext";
 export {subscribeUntil} from "./util/rxjs-helper";
 export {MicroserviceTestApp} from "./util/test-app";
